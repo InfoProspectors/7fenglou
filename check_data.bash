0e7511a0f0cd0ac7c8b2d04fc7ce6d5717496130
h7ml <h7ml@qq.com>
2023-07-05 21:17:12 +0800

    feat: check_data

--- check_data.bash
+++ check_data.bash
@@ -2,7 +2,7 @@
 
 url="$1"
 if [[ -z "$url" ]]; then
-  echo "URL is missing. Usage: sh check_data.sh <url>"
+  echo "URL is missing. Usage: bash check_data.sh <url>"
   exit 1
 fi
 
@@ -15,8 +15,28 @@ echo "Making API request..."
 response=$(curl -s "$url" | jq -Rs '.')
 echo "$response" > response.json
 
+# Check if the response is a valid JSON object
+jq -e . >/dev/null response.json
+if [ $? -ne 0 ]; then
+  echo "Invalid JSON response"
+
+  # Download the JSON file from the specified URL and save it as response.json
+  echo "Downloading response.json from URL..."
+  curl -s -o response.json "https://raw.githubusercontent.com/InfoProspectors/7fenglou/main/data/7fenglou1.json"
+
+  # Check if the download was successful
+  if [ $? -ne 0 ]; then
+    echo "Failed to download the JSON file"
+    exit 1
+  fi
+
+  echo "response.json downloaded successfully"
+fi
+
+
 # Extract the 'totalPage' field from the response and store it in 'total_page' variable
-total_page=$(jq -r '.Data.totalPage // empty' response.json)
+# Read the 'totalPage' field again from the file
+total_page=$(jq -r '.Data.totalPage' response.json | select(. != null))
 if [[ -z "$total_page" ]]; then
   echo "Total pages field not found or is null"
   exit 1
@@ -27,20 +47,14 @@ echo "Total pages: $total_page"
 if [ "$file_count" -ne "$total_page" ]; then
   echo "Mismatch: File count does not match total pages"
   echo "Removing log and data directories..."
-   rm -rf log
-   rm -rf data
+#   rm -rf log
+#   rm -rf data
 else
   echo "File count matches total pages"
 fi
 
-# Read the 'totalPage' field again from the file
-total_page=$(jq -r '.Data.totalPage' response.json | select(. != null))
-if [[ -z "$total_page" ]]; then
-  echo "Total pages field not found or is null"
-  exit 1
-fi
-echo "Total pages: $total_page"
+
 
 # Clean up the response file
-rm response.json
-echo "Response file removed"
+#rm response.json
+#echo "Response file removed"
